1ea156767b1341baf6a9f8436ee4d4c254571a8f
mPooja-1507 <mishrapooja8128@gmail.com>
2023-02-23 19:04:46 +0530

    Fix some css

--- src/PecuScan/AddBlock/Blockcontent.js
+++ src/PecuScan/AddBlock/Blockcontent.js
@@ -1,47 +1,46 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import NorthEastOutlinedIcon from '@mui/icons-material/NorthEastOutlined';
 
 function Blockcontent() {
+
+  const card = [
+    {
+      Name: "Network Utilization (24H)",
+      Rate: "54.5%",
+    },
+    {
+      Name: "Last Safe Block",
+      Rate: 16683092,
+    },
+    {
+      Name: "Average Gas Price (24h)",
+      Rate: "37.349599727 Gwei",
+    },
+    {
+      Name: "Burnt Fees 🔥",
+      Rate: "2,931,295.61 ETH",
+    }
+  ]
   return (
-    <section className="container">
-      <Grid container spacing={2} columns={12} className="blockContent">
-        <Grid item xs={3} className="contentCard">
-          <Box>
-            <h6>Network Utilization (24H)</h6>
-          </Box>
-          <Box>
-            <span>54.5%</span>
-          </Box>
-        </Grid>
-
-        <Grid item xs={3} className="contentCard">
-          <Box>
-            <h6>Last Safe Block</h6>
-          </Box>
-          <Box>
-            <span>16683092</span>
-          </Box>
-        </Grid>
-
-        <Grid item xs={3} className="contentCard">
-          <Box>
-            <h6>Average Gas Price (24h)</h6>
-          </Box>
-          <Box>
-            <span>37.349599727 Gwei</span>
-          </Box>
-        </Grid>
-
-        <Grid item xs={3} className="contentCard">
-          <Box>
-            <h6>Burnt Fees 🔥</h6>
-          </Box>
-          <Box>
-            <span>2,931,295.61 ETH</span>
-          </Box>
-        </Grid>
-      </Grid>
+    <section className="container" style={{ padding: "0" }}>
+      <Box className="mainContent">
+        {card.map((data) => {
+          return (
+            <Card className="contentCard blockContent">
+              <Typography className="mainHead">
+                <span>{data.Name}</span>
+                <span ><i><NorthEastOutlinedIcon /></i></span>
+              </Typography>
+              <Typography style={{ fontSize: "18px" }} className="hoverContent">
+                {data.Rate}
+              </Typography>
+            </Card>
+          )
+        })}
+      </Box>
     </section>
   );
 }
--- src/PecuScan/AddBlock/Newblock.js
+++ src/PecuScan/AddBlock/Newblock.js
@@ -1,15 +1,13 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import { Button, Typography } from "@mui/material";
+import TipsAndUpdatesOutlinedIcon from '@mui/icons-material/TipsAndUpdatesOutlined';
+import TablePagination from '@mui/material/TablePagination';
 
 const columns = [
   { id: "Block", label: "Block" },
@@ -38,74 +36,111 @@ function createData(
 }
 
 const rows = [
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
-  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)" ),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
+  createData(16683340, "6 secs ago", 122, "Flashbots: Builder", "15,498,848(51.66%|+3%)", "30,000,000", "26.5 Gwei", "0.02158 ETH", "0.410808 (-26.21%)"),
 ];
 
 
 function Newblock() {
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
     <>
-      <section className="container">
-        <div className="blockCard">
-          <div className="cardHeader">
-            <div style={{ padding: "16px" }}>
-              <div>
-                <span>Total of 16,683,133 blocks</span>
-              </div>
-              <div>
-                <span>(Showing blocks between #16683108 to #16683132)</span>
+      <section className="container" style={{ padding: "0" }}>
+        <div style={{ paddingBottom: "48px" }}>
+          <div className="blockCard">
+            <div className="cardHeader">
+              <div style={{ padding: "16px" }}>
+                <div>
+                  <span>Total of 16,683,133 blocks</span>
+                </div>
+                <div>
+                  <p>(Showing blocks between #16683108 to #16683132)</p>
+                </div>
               </div>
             </div>
+            <Paper sx={{ width: "100%", overflow: "hidden" }}>
+              <TableContainer>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow>
+                      {columns.map((column) => (
+                        <TableCell
+                          key={column.id}
+                          align={column.align}
+                          style={{ minWidth: column.minWidth }}
+                          className="tableHead"
+                        >
+                          {column.label}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((data) => {
+                      return (
+                        <>
+                          <TableRow hover>
+                            <TableCell><a style={{ color: "rgb(7, 132, 195)" }}>{data.Block}</a></TableCell>
+                            <TableCell>{data.Age}</TableCell>
+                            <TableCell><a style={{ color: "rgb(7, 132, 195)" }}>{data.Txn}</a></TableCell>
+                            <TableCell><a style={{ color: "rgb(7, 132, 195)" }}>{data.Recipient}</a></TableCell>
+                            <TableCell>{data.Used}</TableCell>
+                            <TableCell>{data.Limit}</TableCell>
+                            <TableCell>{data.Fee}</TableCell>
+                            <TableCell>{data.Reward}</TableCell>
+                            <TableCell>{data.Burnt}</TableCell>
+                          </TableRow>
+                        </>
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </Paper>
           </div>
-          <Paper sx={{ width: "100%", overflow: "hidden" }}>
-            <TableContainer>
-              <Table stickyHeader aria-label="sticky table">
-                <TableHead>
-                  <TableRow>
-                    {columns.map((column) => (
-                      <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ minWidth: column.minWidth }}
-                        className="tableHead"
-                      >
-                        {column.label}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((data) => {
-                    return (
-                      <>
-                        <TableRow hover>
-                              <TableCell><a>{data.Block}</a></TableCell>
-                              <TableCell>{data.Age}</TableCell>
-                              <TableCell><a>{data.Txn}</a></TableCell>
-                              <TableCell><a>{data.Recipient}</a></TableCell>
-                              <TableCell>{data.Used}</TableCell>
-                              <TableCell>{data.Limit}</TableCell>
-                              <TableCell>{data.Fee}</TableCell>
-                              <TableCell>{data.Reward}</TableCell>
-                              <TableCell>{data.Burnt}</TableCell>
-                        </TableRow>
-                      </>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Paper>
+          <div style={{ marginTop: "16px" }}>
+            <p>
+              <i className="lightIcon"><TipsAndUpdatesOutlinedIcon /></i>
+              <span>Blocks are batches of transactions linked together via cryptographic hashes. Any tampering of a block invalidates subsequent blocks as their hashes would be changed. Learn more about this page in our
+                <a style={{ color: "rgb(7, 132, 195)" }}> Knowledge Base</a>.
+              </span>
+            </p>
+          </div>
         </div>
       </section>
     </>
